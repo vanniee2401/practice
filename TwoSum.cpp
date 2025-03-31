caa788adf5327f3238b39397c0d5d4342cfac90e
Tuong Van <nntv2401@gmail.com>
2025-03-31 13:19:03 +0700

    first commit

---
+++ TwoSum.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int N = nums.size();
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
