d63106535bb208a3d259b5a9cb5d4bbde88fcb9b
Tuong Van <nntv2401@gmail.com>
2025-04-25 14:23:33 +0700

    update solution 3

---
+++ LongestSubString.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_set<char> chars;
+        int left = 0, right = 0, maxLen = 0;
+
+        while (right < s.length())
+        {
+            if (chars.find(s[right]) == chars.end())
+            {
+                chars.insert(s[right]);
+                maxLen = max(maxLen, right - left + 1);
+                right++;
+            }
+            else
+            {
+                chars.erase(s[left]);
+                left++;
+            }
+        }
+        return maxLen;
+    }
+};
